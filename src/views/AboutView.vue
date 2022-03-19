4b00fca90578dc3d8aa05a4bd9b89acec2454fe3
BamiPremier <87478487+BamiPremier@users.noreply.github.com>
2022-03-19 10:38:17 +0100

    VueJs

---
+++ src/views/AboutView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+       <AboutWorld msg="Welcome to Your Vue.js App About"/>
+ 
+  </div>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import AboutWorld from '@/components/AboutComponent.vue'
+
+export default {
+  name: 'AboutView',
+  components: {
+    AboutWorld
+  }
+}
+</script>
